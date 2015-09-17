f25c7ec0a2ae7bce86a0805049b67b228ae06788
thomellis <tehtom@gmail.com>
2015-09-17 16:38:00 -0700

    make close work

--- js/app.js
+++ js/app.js
@@ -10,10 +10,17 @@ $(function () {
         $.ajax({
             url: "http://games.espn.go.com/ffl/format/playerpop/overview?leagueId=337385&playerId=" + id + "&playerIdType=playerId&seasonId=2015",
             success: function (result) {
-                result = result + '<div class="pc_foot"><img src="../img/close.gif" width="71" height="20" border="0" alt="CLOSE" class="close_btn"></div>';
+                result = result + '<div class="pc_foot"><img onclick="test();" src="../img/close.gif" width="71" height="20" border="0" alt="CLOSE" class="close_btn"></div>';
                 elem.attr('data-original-title', result);
             }
         });
         $(this).tooltip();
     });
+
 });
+
+function test() {
+    $('div.in').each(function() {
+        $(this).tooltip('hide');
+    });
+}
